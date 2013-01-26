430dfc37c1ed2e5e190e92ba3ec6fec40506449f
Michael Henderson <quoha@quoha.org>
2013-01-25 22:31:32 -0700

    Initial Commit

---
+++ Glowing Archer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Glowing Archer
+//
+//  Created by Michael Henderson on 1/25/13.
+//  Copyright (c) 2013 Michael Henderson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
